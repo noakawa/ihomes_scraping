1172cd7ac8f62381f409c75a80e208f83165c85c
Noa Kawa <noakawa@gmail.com>
2021-11-08 15:43:11 +0200

    calls to config file

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 URL = 'https://www.ihomes.co.il/s/tel-aviv-yafo?page='
 PAGES = 51
 BATCHES = 10
+HE_TO_EN = {'פנטהאוז': 'Penthouse', 'בית': 'House', 'דירת גן': 'Garden Apartment', 'דופלקס': 'Duplex',
+            'דו משפחתי': 'Semi-detatched', 'וילה': 'Villa', "קוטג'": 'Cottage', 'מגרש': 'Plot', 'סטודיו': 'Studio',
+            'משרד': 'Office', 'מיני פנטהוז': 'Mini Penthouse', 'דירה': 'Apartment'}
+ATTRIBUTES = ['Price', 'Sale or Rent ?', 'Condition', 'Type of property', 'Floors in building', 'Floor', 'Rooms']
