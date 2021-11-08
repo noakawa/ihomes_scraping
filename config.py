4844014bb8c6ecb4523bb768a657c7bda619387f
Noa Kawa <noakawa@gmail.com>
2021-11-08 17:13:55 +0200

    logging and try except

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ PAGES = 51
 BATCHES = 10
 HE_TO_EN = {'פנטהאוז': 'Penthouse', 'בית': 'House', 'דירת גן': 'Garden Apartment', 'דופלקס': 'Duplex',
             'דו משפחתי': 'Semi-detatched', 'וילה': 'Villa', "קוטג'": 'Cottage', 'מגרש': 'Plot', 'סטודיו': 'Studio',
-            'משרד': 'Office', 'מיני פנטהוז': 'Mini Penthouse', 'דירה': 'Apartment'}
+            'משרד': 'Office', 'מיני פנטהוז': 'Mini Penthouse', 'דירה': 'Apartment', 'בנין': 'building'}
 ATTRIBUTES = ['Price', 'Sale or Rent ?', 'Condition', 'Type of property', 'Floors in building', 'Floor', 'Rooms']
