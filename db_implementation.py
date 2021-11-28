603993a802caa65842d8a072c62c5ab023bef616
Noa Kawa <noakawa@gmail.com>
2021-11-28 19:09:16 +0200

    insert functions

--- db_implementation.py
+++ db_implementation.py
@@ -11,7 +11,32 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-mycursor.execute()
+
+def insert_city(city):
+    mycursor.execute(f''' INSERT INTO Cities 
+        (city_name)
+        VALUES ({city})''')
+
+
+def insert_type(type_of_property):
+    mycursor.execute(f''' INSERT INTO Cities 
+        (type_of_property)
+        VALUES ({type_of_property})''')
+
+
+def insert_property(link, sale_or_rent, condition, type_of_property_id, floor_in_building, floor, rooms, built_area,
+                    furnished, first_listed, city_id):
+    mycursor.execute(f''' INSERT INTO property 
+    (link, sale_or_rent, condition, type_of_property_id, floor_in_building, floor, rooms, built_area, furnished, 
+    first_listed, city_id)
+    VALUES ({link}, {sale_or_rent}, {condition}, {type_of_property_id}, {floor_in_building}, {floor}, {rooms}, {built_area}, 
+            {furnished}, {first_listed}, {city_id})''')
+
+
+def insert_price(property_id, date_of_today, price):
+    mycursor.execute(f''' INSERT INTO Cities 
+        (property_id, date_of_today, price)
+        VALUES ({property_id}, {date_of_today}, {price})''')
 
 # data = os.system("python web_scrap.py -c ak")
 # print(data)
