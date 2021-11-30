1687847a56f5b4496eac4060db3df64ea7770157
kerenportugais <93246398+kerenportugais@users.noreply.github.com>
2021-11-30 09:56:24 +0200

    docstring-implementation

--- db_implementation.py
+++ db_implementation.py
@@ -13,6 +13,9 @@ mycursor = mydb.cursor()
 
 
 def insert_city(city):
+    """input: city name
+    Insert city in table City in row city_name"""
+    
     if len(get_city_id(city)) == 0:
         mycursor.execute(''' INSERT INTO Cities 
             (city_name)
@@ -22,6 +25,9 @@ def insert_city(city):
 
 
 def insert_type(type_of_property):
+    """input: type_of_property
+    Insert type_of_property in table Type_of_property in row type_of_property"""
+        
     if len(get_type_of_property_id(type_of_property)) == 0:
         mycursor.execute(''' INSERT INTO Type_of_property
             (type)
@@ -32,6 +38,10 @@ def insert_type(type_of_property):
 
 def insert_property(link, sale_or_rent, type_of_property_id, floor_in_building, floor, rooms, built_area,
                     furnished, first_listed, city_id, conditions):
+    """input: link, sale_or_rent, type_of_property_id, floor_in_building, floor, rooms, built_area,
+                    furnished, first_listed, city_id, conditions
+    Insert into table Property the input information"""
+    
     if len(get_property_id(link)) == 0:
         query = ''' INSERT INTO Property 
         (link, sale_or_rent, type_of_property_id, floor_in_building, floor, rooms, built_area,
@@ -47,6 +57,9 @@ def insert_property(link, sale_or_rent, type_of_property_id, floor_in_building, 
 
 
 def insert_price(property_id, date_of_today, price):
+    """input: property_id, date_of_today, price from the scrap data
+    Insert the input data to table Price """
+    
     if len(get_price_id(property_id, price)) == 0:
         query = ''' INSERT INTO Price 
             (property_id, date_of_today, price)
@@ -58,18 +71,27 @@ def insert_price(property_id, date_of_today, price):
 
 
 def get_city_id(city):
+    """input: city name
+    Return the correspong id of city from table Cities in the database"""
+    
     mycursor.execute(f''' SELECT id FROM Cities 
             WHERE city_name = ('%s')''' % city)
     return mycursor.fetchall()
 
 
 def get_type_of_property_id(prop_type):
+    """input: a type of property
+    Return it's ID from table Type_of_property"""
+    
     mycursor.execute(f''' SELECT id FROM Type_of_property
             WHERE type = ('%s')''' % prop_type)
     return mycursor.fetchall()
 
 
 def get_property_id(link):
+    """input: link of a property
+    Return it's id from table Property"""
+    
     mycursor.execute(f''' SELECT id FROM Property
             WHERE link = ('%s')''' % link)
 
@@ -77,6 +99,9 @@ def get_property_id(link):
 
 
 def get_price_id(property_id, price):
+    """input: id of a property and it's price
+    Return it's id from table Price"""
+    
     mycursor.execute(f''' SELECT id FROM Price
                 WHERE property_id = ('%s') and price = ('%s') ''' % (property_id, price))
 
