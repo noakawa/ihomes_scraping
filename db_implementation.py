79c5d9f842998050a002a4c149d5e217ba587de1
Noa Kawa <noakawa@gmail.com>
2021-11-30 15:43:02 +0200

    no bugs

--- db_implementation.py
+++ db_implementation.py
@@ -1,5 +1,4 @@
 import mysql.connector
-import os
 import private_info
 
 mydb = mysql.connector.connect(
@@ -17,7 +16,7 @@ def insert_city(city):
     This functions inserts a row in the table City if the city does not exist.
     :param city: name of the city
     """
-    
+
     if len(get_city_id(city)) == 0:
         mycursor.execute(''' INSERT INTO Cities 
             (city_name)
@@ -31,7 +30,7 @@ def insert_type(type_of_property):
     This functions inserts a row in the table Type_of_property if the type_of_property does not exist.
     :param type_of_property: type of property
     """
-        
+
     if len(get_type_of_property_id(type_of_property)) == 0:
         mycursor.execute(''' INSERT INTO Type_of_property
             (type)
@@ -42,13 +41,11 @@ def insert_type(type_of_property):
 
 def insert_property(link, sale_or_rent, type_of_property_id, floor_in_building, floor, rooms, built_area,
                     furnished, first_listed, city_id, conditions):
-        """
-    This functions inserts a row in the table Property if the property that corresponds to the link does not exist in the table.
-    :param link, sale_or_rent, type_of_property_id, floor_in_building, floor, rooms, built_area,
-           furnished, first_listed, city_id, conditions
-           
     """
-        
+    This functions inserts a row in the table Property if the property that corresponds to the link does not exist in
+    the table.
+    """
+
     if len(get_property_id(link)) == 0:
         query = ''' INSERT INTO Property 
         (link, sale_or_rent, type_of_property_id, floor_in_building, floor, rooms, built_area,
@@ -65,13 +62,13 @@ def insert_property(link, sale_or_rent, type_of_property_id, floor_in_building, 
 
 def insert_price(property_id, date_of_today, price):
     """
-    This functions inserts a row in the table Price if the input price is different from the price that corresponds to the input property id 
-    and update the date of today according to the input date_of_today.
+    This functions inserts a row in the table Price if the input price is different from the price that corresponds
+    to the input property id and update the date of today according to the input date_of_today.
     :param property_id: id of the property
-           date_of_today: today's date
-           price: price of the property
+    :param date_of_today: today's date
+    :param price: price of the property 
     """
-    
+
     if len(get_price_id(property_id, price)) == 0:
         query = ''' INSERT INTO Price 
             (property_id, date_of_today, price)
@@ -87,7 +84,7 @@ def get_city_id(city):
     This functions return the id of the corresponding city in the table Cities.
     :param city: name of the city
     """
-    
+
     mycursor.execute(f''' SELECT id FROM Cities 
             WHERE city_name = ('%s')''' % city)
     return mycursor.fetchall()
@@ -98,7 +95,7 @@ def get_type_of_property_id(prop_type):
     This functions return the id from table Type_of_property according to the given type of property.
     :param prop_type: type of property
     """
-    
+
     mycursor.execute(f''' SELECT id FROM Type_of_property
             WHERE type = ('%s')''' % prop_type)
     return mycursor.fetchall()
@@ -109,7 +106,7 @@ def get_property_id(link):
     This functions return the id from table Property according to the given type of link.
     :param link: link of a property
     """
-        
+
     mycursor.execute(f''' SELECT id FROM Property
             WHERE link = ('%s')''' % link)
 
@@ -117,14 +114,14 @@ def get_property_id(link):
 
 
 def get_price_id(property_id, price):
-     """
-    This functions return the id from table Price according to the given property id and price.
-    :param property_id: id of property
-           price: price of a property
     """
+   This functions return the id from table Price according to the given property id and price.
+   :param property_id: id of property
+   :param price: price of a property
+   """
 
     mycursor.execute(f''' SELECT id FROM Price
-                WHERE property_id = ('%s') and price = ('%s') ''' % (property_id, price))
+                    WHERE property_id = ('%s') and price = ('%s') ''' % (property_id, price))
 
     return mycursor.fetchall()
 
